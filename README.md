01078ab7a50425aa677b3ced5130c27fbe306622
Steven Serette <sserette@gmail.com>
2018-12-18 13:38:26 +0000

    add tests

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 * Node.js 11.4.0
 
 ### Running instructions
-1. `npm install`
-2. `npm start`
-3. Navigate to localhost:8080
+1. `npm install` (first time)
+2. `npm install -g nodemon` (first time)
+3. `npm start`
+4. Navigate to localhost:8080
---
+++ test/predict.test.js
@@ -0,0 +1,60 @@
+const tap = require('tap');
+const predict = require(`${process.cwd()}/predict.js`);
+
+tap.test('getPrediction returns a prediction', function(t) {
+    t.plan(1);
+    var record = {
+        date: '9/10/2009',
+        gameId: '2009091000',
+        drive: '1',
+        quarter: '1',
+        down: '1',
+        time: '14:53',
+        timeUnder: '15',
+        timeInSec: '3593',
+        playTimeDiff: '7',
+        side: 'PIT',
+        yardLine: '42',
+        yardsFromOwnGoal: '58',
+        yardsToGo: '10',
+        netYards: '5',
+        goalToGo: '0',
+        firstDown: '0',
+        possessingTeam: 'PIT',
+        defendingTeam: 'TEN',
+        yardsGained: '5',
+        touchdown: '0',
+        safety: '0',
+        tackler1: 'C. Hope',
+        tackler2: 'NA',
+        fumble: '0',
+        fumbleRecoveringTeam: 'NA',
+        fumbleRecoveringPlayer: 'NA',
+        possScore: '0',
+        defScore: '0',
+        scoreDiff: '0',
+        homeTeam: 'PIT',
+        awayTeam: 'TEN',
+        homeTimeouts: '3',
+        awayTimeouts: '3',
+        fieldGoalProb: '0.293699785',
+        safetyProb: '0.00363751',
+        touchdownProb: '0.423011386',
+        winProb: '0.54643284',
+        winProbAdded: '0.004654977',
+        season: '2009'
+    };
+    return predict.getPrediction(record).then(function(result) {
+        t.ok(result.hasOwnProperty('Prediction'), 'prediction is set');
+        t.end();
+    });
+});
+
+tap.test('getPrediction returns even with empty record object', function(t) {
+    t.plan(1);
+    var record = {};
+    return predict.getPrediction(record).then(function(result) {
+        t.ok(result.hasOwnProperty('Prediction'), 'prediction is set');
+        t.end();
+    });
+});
